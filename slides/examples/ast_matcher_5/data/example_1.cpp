d9d0abee6a458621215b8323205682adc290e1cf
Michael Adams <mdadams@ece.uvic.ca>
2023-02-28 22:21:57 -0800

    Synchronize files with the official upstream source.

---
+++ slides/examples/ast_matcher_5/data/example_1.cpp
@@ -0,0 +1,9 @@
+class Widget {
+};
+
+constexpr int forty_two() {return 42;}
+
+int main() {
+	Widget w;
+	int i = forty_two();
+}
---
+++ slides/examples/ast_matcher_5/main.cpp
@@ -0,0 +1,79 @@
+#include <format>
+#include <string>
+#include "clang/ASTMatchers/ASTMatchers.h"
+#include "clang/ASTMatchers/ASTMatchFinder.h"
+#include "clang/ASTMatchers/Dynamic/VariantValue.h"
+#include "clang/AST/Type.h"
+#include "clang/Frontend/FrontendActions.h"
+#include "clang/Tooling/CommonOptionsParser.h"
+#include "clang/Tooling/Tooling.h"
+#include "llvm/Support/CommandLine.h"
+
+namespace ct = clang::tooling;
+namespace cam = clang::ast_matchers;
+namespace lc = llvm::cl;
+
+cam::dynamic::VariantMatcher getMatcher(int id) {
+	using namespace cam;
+	switch (id) {
+	default:
+	case 0:
+		return dynamic::VariantMatcher::SingleMatcher(qualType().bind("qt"));
+	case 1:
+		return dynamic::VariantMatcher::SingleMatcher(varDecl().bind("v"));
+	case 2:
+		return dynamic::VariantMatcher::SingleMatcher(
+		  callExpr(callee(functionDecl().bind("f"))));
+	}
+}
+
+struct MyMatchCallback : public cam::MatchFinder::MatchCallback {
+	MyMatchCallback() : count(0) {}
+	void run(const cam::MatchFinder::MatchResult& result) override;
+	unsigned count;
+};
+
+void MyMatchCallback::run(const cam::MatchFinder::MatchResult& result) {
+	llvm::outs() << std::format("MATCH {}:\n", count);
+	if (auto qualTypePtr = result.Nodes.getNodeAs<clang::QualType>("qt")) {
+		llvm::outs() << "dump for QualType:\n";
+		qualTypePtr->dump();
+	} else if (auto varDecl =
+	  result.Nodes.getNodeAs<clang::VarDecl>("v")) {
+		std::string s(varDecl->getQualifiedNameAsString());
+		llvm::outs() << std::format("dump for VarDecl {}:\n",
+		  !s.empty() ? s : "[unnamed]");
+		varDecl->dump();
+	} else if (auto funcDecl =
+	  result.Nodes.getNodeAs<clang::FunctionDecl>("f")) {
+		llvm::outs() << std::format("dump for FunctionDecl {}:\n",
+		  funcDecl->getQualifiedNameAsString());
+		funcDecl->dump();
+	}
+	++count;
+}
+
+static lc::OptionCategory optionCategory("Tool options");
+static lc::list<int> clMatcherIds("m", lc::desc("Matcher ID"),
+  lc::cat(optionCategory), lc::ZeroOrMore);
+
+int main(int argc, const char **argv) {
+	auto optParser = ct::CommonOptionsParser::create(argc, argv,
+	  optionCategory);
+	if (!optParser) {
+		llvm::errs() << llvm::toString(optParser.takeError());
+		return 1;
+	}
+	ct::ClangTool tool(optParser->getCompilations(),
+	  optParser->getSourcePathList());
+	MyMatchCallback matchCallback;
+	cam::MatchFinder matchFinder;
+	for (auto id : clMatcherIds) {
+		matchFinder.addDynamicMatcher(*getMatcher(id).getSingleMatcher(),
+		  &matchCallback);
+	}
+	int status = tool.run(ct::newFrontendActionFactory(&matchFinder).get());
+	llvm::outs() << std::format("number of matches: {}\n",
+	  matchCallback.count);
+	return !status ? 0 : 1;
+}
