9e10b88521fa5f1b2a9d268a412e6f8ce86b780d
Michael Adams <mdadams@ece.uvic.ca>
2023-04-17 09:50:50 -0700

    Synchronize files with the official upstream source.

--- slides/examples/cmake/ClangSetup.cmake
+++ slides/examples/cmake/ClangSetup.cmake
@@ -3,7 +3,17 @@ function(cl_get_clang_libs out_libs_var)
 		# Note: This case handles the LLVM/Clang distribution in Fedora 37.
 		list(APPEND libs LLVM clang-cpp libclang)
 	else()
-		list(APPEND libs clangTooling libclang)
+		list(APPEND libs
+			clangTooling
+			clangAST
+			clangASTMatchers
+			clangBasic
+			clangDynamicASTMatchers
+			clangFrontend
+			clangTooling
+			clangSerialization
+			libclang
+		)
 	endif()
 	message("Clang libraries: ${libs}")
 	set(${out_libs_var} ${libs} PARENT_SCOPE)
